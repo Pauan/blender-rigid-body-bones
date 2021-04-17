05c95c175e193a2b1c4583a08374b02dca928b57
Pauan <pauanyu+github@pm.me>
2021-04-17 19:51:13 +0200

    Version 1.4.1

--- Rigid Body Bones/__init__.py
+++ Rigid Body Bones/__init__.py
@@ -16,7 +16,7 @@
 bl_info = {
     "name": "Rigid Body Bones",
     "author": "Pauan",
-    "version": (1, 4),
+    "version": (1, 4, 1),
     # Minimum version because of https://developer.blender.org/T81345
     "blender": (2, 91, 0),
     "location": "View3D > Sidebar > Rigid Body Bones",
