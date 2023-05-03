da28b44b085046400b1049c500c61246fcb9d913
Pauan <pauanyu+github@pm.me>
2023-05-02 19:18:28 -0700

    Updating install script

--- install.py
+++ install.py
@@ -2,18 +2,19 @@ import bpy
 import os
 import zipfile
 
-bpy.ops.preferences.addon_disable(module="Rigid Body Bones")
+name = "Rigid Body Bones"
+
+#bpy.ops.preferences.addon_disable(module=name)
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-zip_path = os.path.join(dir_path, "Rigid Body Bones.zip")
+zip_path = os.path.join(dir_path, name + ".zip")
 
 with zipfile.ZipFile(zip_path, "w") as zip:
-    for root, dirs, files in os.walk(os.path.join(dir_path, "Rigid Body Bones")):
+    for root, dirs, files in os.walk(os.path.join(dir_path, name)):
         for file in files:
             file_path = os.path.join(root, file)
             zip.write(file_path, arcname=os.path.relpath(file_path, start=dir_path))
 
 bpy.ops.preferences.addon_install(filepath=zip_path, overwrite=True)
-bpy.ops.preferences.addon_enable(module="Rigid Body Bones")
-bpy.ops.wm.save_userpref()
+#bpy.ops.preferences.addon_enable(module=name)
